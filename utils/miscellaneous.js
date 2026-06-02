12ec63ad76f655c7d3f9dc3341998e846b97a905
Simone-Sangiovanni <project.devel@protonmail.com>
2026-06-02 18:39:03 +0200

    Move here two functions: formatLevelName and isValidLevel, that previously where in server.js

--- utils/miscellaneous.js
+++ utils/miscellaneous.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 
+
+
 /**
  * Returns a list of all valid level identifiers by scanning the `levels/config` directory.
  * A valid level is any `.json` file in that directory (the filename without extension is used as the level ID).
@@ -20,4 +22,29 @@ function getValidLevels() {
     }
 }
 
-module.exports = { getValidLevels };
+
+/**
+ * Returns a formatted level name for display purposes.
+ * @param {string} levelId - e.g. "level_1" or "level_2"
+ * @returns {string} Human readable level name
+ */
+const formatLevelName = (levelId) => {
+    return levelId
+        .replace(/_/g, ' ')
+        .replace(/level (\d)/i, 'Level $1');
+};
+
+
+/**
+ * Validates that the requested level exists.
+ * @param {string} levelId
+ * @returns {boolean}
+ */
+const isValidLevel = (levelId) => {
+    const validLevels = getValidLevels();
+    return validLevels.includes(levelId);
+};
+
+
+
+module.exports = { getValidLevels, formatLevelName, isValidLevel };
